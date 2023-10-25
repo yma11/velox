479a29a3e1ae38940b3f186e9ff88b5177446ed0
PHILO-HE <feilong.he@intel.com>
2023-10-25 11:29:37 +0800

    cast null as string

--- velox/type/Type.h
+++ velox/type/Type.h
@@ -1429,10 +1429,6 @@ std::shared_ptr<const OpaqueType> OPAQUE() {
         return TEMPLATE_FUNC<::facebook::velox::TypeKind::TIMESTAMP>(         \
             __VA_ARGS__);                                                     \
       }                                                                       \
-      case ::facebook::velox::TypeKind::UNKNOWN: {                            \
-        return TEMPLATE_FUNC<::facebook::velox::TypeKind::UNKNOWN>(           \
-            __VA_ARGS__);                                                     \
-      }                                                                       \
       default:                                                                \
         VELOX_FAIL(                                                           \
             "not a scalar type! kind: {}", mapTypeKindToName(typeKind));      \
