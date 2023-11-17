ddcf0ea7dead2494182f64e13ca6f46ccd82c035
rui-mo <rui.mo@intel.com>
2023-11-17 10:44:32 +0800

    Support timestamp units in arrow bridge (7625)

--- velox/core/QueryConfig.h
+++ velox/core/QueryConfig.h
@@ -343,6 +343,10 @@ class QueryConfig {
   static constexpr const char* kEnableExpressionEvaluationCache =
       "enable_expression_evaluation_cache";
 
+  // Timestamp unit used during Velox-Arrow conversion.
+  static constexpr const char* kArrowBridgeTimestampUnit =
+      "arrow_bridge_timestamp_unit";
+
   uint64_t queryMaxMemoryPerNode() const {
     return toCapacity(
         get<std::string>(kQueryMaxMemoryPerNode, "0B"), CapacityUnit::BYTE);
@@ -558,6 +562,13 @@ class QueryConfig {
   uint8_t spillStartPartitionBit() const {
     constexpr uint8_t kDefaultStartBit = 29;
     return get<uint8_t>(kSpillStartPartitionBit, kDefaultStartBit);
+  }
+
+  /// Returns the timestamp unit used in Velox-Arrow conversion.
+  /// 0: second, 3: milli, 6: micro, 9: nano.
+  uint8_t arrowBridgeTimestampUnit() const {
+    constexpr uint8_t kDefaultUnit = 9;
+    return get<uint8_t>(kArrowBridgeTimestampUnit, kDefaultUnit);
   }
 
   /// Returns the number of bits used to calculate the spilling partition
